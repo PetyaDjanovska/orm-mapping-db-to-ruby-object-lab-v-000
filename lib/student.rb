dd96deff50d8010a6dafec11cb2e452f2b3734d8
Petya Djanovska <petya.djanovska@gmail.com>
2018-03-19 19:53:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -65,4 +65,13 @@ class Student
 
     DB[:conn].execute(sql)
   end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+    SELECT COUNT (id) FROM students WHERE students.grade < 12
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
 end
