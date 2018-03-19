b8f63932d7fcd546845628f2571422c42b37dcbf
Petya Djanovska <petya.djanovska@gmail.com>
2018-03-19 23:31:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,10 +76,10 @@ class Student
 
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-    SELECT COUNT (id) FROM students WHERE students.grade = 9
-    SQL 
-
-    DB[:conn].execute(sql)
+    SELECT * FROM students WHERE students.grade = 10 LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x)
   end
 
 end
